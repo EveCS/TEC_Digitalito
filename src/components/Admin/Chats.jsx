c14cb754f9ceca543bc854528e2796473b59a69a
JhonnASC <93461702+JhonnASC@users.noreply.github.com>
2023-10-02 22:07:51 -0600

    chats full working

--- src/components/Admin/Chats.jsx
+++ src/components/Admin/Chats.jsx
@@ -8,7 +8,7 @@ export function Chats() {
     const [newMessage, setNewMessage] = useState(""); // Estado para el nuevo mensaje
     const [chatsData,setChatsData] = useState([]); // 
     const [participantes,setParticipantes] = useState([]);
-
+    const [destinatario, setDestinatario] = useState("");
     const location = useLocation();
   const username = location.state.usuario;
  
@@ -33,14 +33,21 @@ export function Chats() {
 
         const response = await getParticipantes(chatTitle);
         setParticipantes(response);
-        const destinatario = participantes.find(participant => participant !== username);
+        const enviarA = participantes.find(participant => participant !== username);
 
-        sendMessage(username,destinatario,newMessage);
+        sendMessage(username,enviarA,newMessage);
         setParticipantes("");
       // Aquí debes implementar la lógica de envío de mensaje con tu API
       // Puedes usar 'newMessage' para obtener el contenido del mensaje y 'chatTitle' para saber a qué chat se envía.
       console.log(`Enviando mensaje '${newMessage}' al chat '${chatTitle}'`);
       // Lógica de envío de mensaje con tu API
+    };
+
+    const handleNewContacto = async  (destinatario) => {
+
+      sendMessage(username,destinatario,newMessage);
+      console.log(`Enviando mensaje '${newMessage}' al chat '${destinatario}'`);
+   
     };
   
     const groupedChats = {};
@@ -53,40 +60,65 @@ export function Chats() {
         groupedChats[chat.chat] = [chat];
       }
     });
-  
     return (
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <div className="chat-list" style={{ maxWidth: '600px', width: '100%', padding: '20px', background: '#f5f5f5', borderRadius: '10px' }}>
-          {Object.keys(groupedChats).map((chatTitle, index) => (
-            <div key={index} className="chat-card" style={{ marginBottom: '20px', background: '#fff', border: '1px solid #ddd', borderRadius: '8px', padding: '10px' }}>
-              <h3>{chatTitle}</h3>
-              {groupedChats[chatTitle].map((message, messageIndex) => (
-                <div key={messageIndex} className="chat-item" style={{ marginBottom: '10px' }}>
-                  <div className="chat-content" style={{ fontWeight: 'bold' }}>{message.contenido}</div>
-                  <div className="chat-date" style={{ fontSize: '0.8em', color: '#888' }}>{new Date(message.fecha).toLocaleString()}</div>
-                </div>
-              ))}
-              
-              {/* Campo de enviar mensaje */}
-              <div style={{ display: 'flex', marginTop: '10px' }}>
-                <input 
-                  type="text" 
-                  value={newMessage}
-                  onChange={(e) => setNewMessage(e.target.value)}
-                  style={{ flex: '1', padding: '5px', marginRight: '10px' }} 
-                  placeholder="Escribe tu mensaje..." 
-                />
-                <button 
-                  className="btn btn-primary"
-                  onClick={() => handleSendMessage(chatTitle)}
-                >
-                  Enviar
-                </button>
-              </div>
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+          <div style={{ marginBottom: '20px', background: '#fff', border: '1px solid #ddd', borderRadius: '8px', padding: '10px', width: '600px' }}>
+            <h3>Nuevo Mensaje</h3>
+            <div>
+              <input
+                type="text"
+                value={destinatario}
+                onChange={(e) => setDestinatario(e.target.value)}
+                style={{ flex: '1', padding: '5px', marginRight: '10px', width: '80%' }}
+                placeholder="Destinatario"
+              />
+              <input
+                type="text"
+                value={newMessage}
+                onChange={(e) => setNewMessage(e.target.value)}
+                style={{ flex: '1', padding: '5px', marginRight: '10px', width: '80%' }}
+                placeholder="Escribe tu mensaje..."
+              />
+              <button
+                className="btn btn-primary"
+                onClick={() => handleNewContacto(destinatario)}
+              >
+                Enviar
+              </button>
             </div>
-          ))}
+          </div>
+          <div className="chat-list" style={{ maxWidth: '600px', width: '100%', padding: '20px', background: '#f5f5f5', borderRadius: '10px' }}>
+            {Object.keys(groupedChats).map((chatTitle, index) => (
+              <div key={index} className="chat-card" style={{ marginBottom: '20px', background: '#fff', border: '1px solid #ddd', borderRadius: '8px', padding: '10px' }}>
+                <h3>{chatTitle}</h3>
+                {groupedChats[chatTitle].map((message, messageIndex) => (
+                  <div key={messageIndex} className="chat-item" style={{ marginBottom: '10px' }}>
+                    <div className="chat-content" style={{ fontWeight: 'bold' }}>{message.contenido}</div>
+                    <div className="chat-date" style={{ fontSize: '0.8em', color: '#888' }}>{new Date(message.fecha).toLocaleString()}</div>
+                  </div>
+                ))}
+    
+                {/* Campo de enviar mensaje */}
+                <div style={{ display: 'flex', marginTop: '10px' }}>
+                  <input
+                    type="text"
+                    value={newMessage}
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    style={{ flex: '1', padding: '5px', marginRight: '10px' }}
+                    placeholder="Escribe tu mensaje..."
+                  />
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => handleSendMessage(chatTitle)}
+                  >
+                    Enviar
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    );
+      );
+    
   }
   
